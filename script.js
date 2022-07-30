73f3c9d686aab515eceea910f3dec5a466191894
richielollis <richielollisdesktop@richies-mini.lan>
2022-07-30 10:16:27 -0400

    generate password and validation

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 // Assignment code here
-const userOptions = {};
-
+let userOptions = {};
+let optionsArray = [];
+let passwordArray = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  userPrompts();
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
+  userOptions = {}; 
+  optionsArray = [];
+  passwordArray = [];
+
+  userPrompts();
+  generatePassword();
+  
+  var password = passwordArray.join('');
+  var passwordText = document.querySelector("#password");
+  
   passwordText.value = password;
 
+  
 }
 
 // Add event listener to generate button
@@ -30,19 +39,18 @@ const userPrompts = () => {
   userOptions.uppercase = confirm('Would you like your password to include uppercase letters?');
   userOptions.lowercase = confirm('Would you like your password to include lowercase letters?');
   userOptions.numeric = confirm('Would you like your password to include any numbers?');
-  userOptions.specialCharacters = confirm('Would you like your password to include special characters?');
+  userOptions.specialCharacter = confirm('Would you like your password to include special characters?');
 
   console.log(userOptions)
 
-  if (!userOptions.uppercase && !userOptions.lowercase && !userOptions.numeric && !userOptions.specialCharacters) {
+  if (!userOptions.uppercase && !userOptions.lowercase && !userOptions.numeric && !userOptions.specialCharacter) {
     alert('At least one character type muct be chosen.')
     userPrompts();
   };
-
 };
 
 //Character Arrays
-const upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 const lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
@@ -51,12 +59,76 @@ const numericArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const specialCharacterArray = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', "/", "]", '^', '_', '`', '{', '|', '}', '~'];
 
 //Generate Password
+
 const generatePassword = () => {
   if (userOptions.uppercase) {
-  console.log(upperCaseArray[Math.floor(Math.random()*upperCaseArray.length)]);
-  
+    for (let i = 0; i < uppercaseArray.length; i++) {
+      optionsArray.push(uppercaseArray[i])
+    }
   };
+
+  if (userOptions.lowercase) {
+    for (let i = 0; i < lowercaseArray.length; i++) {
+      optionsArray.push(lowercaseArray[i])
+    }
+  };
+
+  if (userOptions.numeric) {
+    for (let i = 0; i < numericArray.length; i++) {
+      optionsArray.push(numericArray[i])
+    }
+  };
+
+  if (userOptions.specialCharacter) {
+    for (let i = 0; i < specialCharacterArray.length; i++) {
+      optionsArray.push(specialCharacterArray[i])
+    }
+  };
+
+  console.log(optionsArray)
+
+  for (let y = 0; y < userOptions.length; y++) {
+    passwordArray.push(optionsArray[Math.floor(Math.random()*optionsArray.length)]);
+  };
+
+  console.log(passwordArray)
+  
+  if (userOptions.uppercase) {
+    const validateUppercase = console.log(passwordArray.some(r=> uppercaseArray.indexOf(r) >= 0))
+    if (validateUppercase === false) {
+      generatePassword()
+    }
+  };
+
+  if (userOptions.lowercase) {
+    const validateLowercase = console.log(passwordArray.some(r=> lowercaseArray.indexOf(r) >= 0))
+    if (validateLowercase === false) {
+      generatePassword()
+    }
+  };
+
+  if (userOptions.numeric) {
+    const validateNumeric = console.log(passwordArray.some(r=> numericArray.indexOf(r) >= 0))
+    if (validateNumeric === false) {
+      generatePassword()
+    }
+  };
+
+  if (userOptions.specialCharacter) {
+    const validateSpecialCharacter = console.log(passwordArray.some(r=> specialCharacterArray.indexOf(r) >= 0))
+    if (validateSpecialCharacter === false) {
+      generatePassword()
+    }
+  };
+
 };
+
+
+
+
+
+
+
 
 
 
